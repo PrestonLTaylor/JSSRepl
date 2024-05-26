75c2a98d75b23df913e309f5694916a075e3000f
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-26 15:39:08 +0100

    JSS-92 Implement a service for getting the latest JSS commit information
    The "GitHubService" provides a function for getting the latest commit
    information from the master branch of JSS.
    
    This uses the Octokit.net package for retrieving the information we
    need.

---
+++ JSSRepl/Application/Services/IGitHubService.cs
@@ -0,0 +1,15 @@
+﻿using Octokit;
+
+namespace Application.Services;
+
+/// <summary>
+/// A service for retrieving commit information from the JSS GitHub repository.
+/// </summary>
+public interface IGitHubService
+{
+    /// <summary>
+    /// Retrieves information on the latest commit from the master branch.
+    /// </summary>
+    /// <returns>A task containing the latest commit from the master branch.</returns>
+    public Task<GitHubCommit> GetLatestCommitAsync();
+}
