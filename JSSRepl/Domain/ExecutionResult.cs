752e4db13f1e27664e420348b3201e50eefc96d7
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-27 21:36:10 +0100

    JSS-94 Display erroneous results with red text

---
+++ JSSRepl/Domain/ExecutionResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+/// <summary>
+/// Represents the result of executing JavaScript from the JSS engine.
+/// </summary>
+/// <param name="Value">The result of execution as a string.</param>
+/// <param name="IsNormalCompletion"><see langword="true"/> if execution was a normal completion, <see langword="false"/> otherwise.</param>
+public record struct ExecutionResult(string Value, bool IsNormalCompletion);
