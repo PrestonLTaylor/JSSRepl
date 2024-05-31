8c1955db35a894c75928fe10902b8fbf6275c221
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-05-31 14:51:13 +0100

    JSS-100 Store execution results in a list of items

---
+++ JSSRepl/Domain/ConsoleItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+/// <summary>
+/// Represents an item to be displayed in the REPL console.
+/// </summary>
+/// <param name="Value">The content to be displayed in the REPL console.</param>
+/// <param name="IsError">If the JSS execution was an abrupt completion then the item should be an error.</param>
+public record struct ConsoleItem(string Value, bool IsError);
