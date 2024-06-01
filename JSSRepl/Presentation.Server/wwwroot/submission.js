164cf818d116295c0331988b5017e281a619e93c
PrestonLTaylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-06-01 14:07:20 +0100

    JSS-101 Execute script when Crtl+Enter is pressed inside the input

---
+++ JSSRepl/Presentation.Server/wwwroot/submission.js
@@ -0,0 +1,9 @@
+﻿function submitForm(element) {
+    // NOTE: This is a hack as the "Script" variable in our REPL does not update when focus isn't changed from the text area when typing inside of it
+    // Forcing the focus away from the TextArea forces the script variable to update and will have the correct value when clicked.
+    // We then restore the focus back to the previous active element for a cleaner user experience.
+    const previousFocus = document.activeElement;
+    element.focus();
+    element.click();
+    previousFocus.focus();
+}
